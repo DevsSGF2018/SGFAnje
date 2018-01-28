f7aaa82b074fb0a793a1b212b5ff5f164188a691
Mouzinho do Santos Zacarias <mouzinhodossantoszacarias@gmail.com>
2018-01-28 20:43:33 +0200

    Initial commit

---
+++ app/src/main/java/com/afrofx/code/moze/RegisterActivity.java
@@ -0,0 +1,12 @@
+package com.afrofx.code.moze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Afro FX on 1/26/2018.
+ */
+
+public class RegisterActivity {
+
+}
